7c6c914286f7da5293aa05fec6e70ed8b6bc3723
HamidReza Kohzadpour <kohzadpour@gmail.com>
2024-04-16 14:35:46 +0330

    Update get_user_last_connection_concurrent.sh

--- get_user_last_connection_concurrent.sh
+++ get_user_last_connection_concurrent.sh
@@ -10,14 +10,14 @@ do
 	        if [ $count_of_session -gt 2 ];
 	        then
 	        	killall -KILL -u $i
-			echo  $(date) "User:" $i "Killed all session and waiting for 30 second." >> /root/vpn_log.txt
+			echo  $(date) "User:" $i "Killed all session and waiting for 30 second." >> /root/log.txt
 			sleep 30
 			count_of_session=` lsof -i TCP -s tcp:established -n | grep ssh | grep $i | grep 1322 | wc -l`
 			if [ $count_of_session -gt 2 ];
 			then
 				killall -KILL -u $i
 				/usr/sbin/usermod -L -e 1 $i
-				echo  $(date) "User:" $i "Killed and Locked!."  >> /root/vpn_log.txt
+				echo  $(date) "User:" $i "Killed and Locked!."  >> /root/log.txt
 			fi
 		fi
 	fi
